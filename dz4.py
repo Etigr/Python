4e9d3875cec99a64f6acf4fd293080eb64539a65
Evgen <fadi_86@mail.ru>
2023-01-12 01:49:27 +0300

    DZ4

---
+++ dz4.py
@@ -0,0 +1,157 @@
+from random import randint
+import math
+
+
+# 1 Вычислить число c заданной точностью d
+# Пример:  при d = 0.001, π = 3.141   10^{-1} ≤ d ≤10^{-10}
+
+# n = int(input('Введите количество знаков после запятой: '))
+# print("значение PI", round(math.pi, n))
+
+# через функцию
+
+# def diskret(n):
+#     count = 0 
+#     while n % 1 != 0:
+#         n *= 10
+#         count += 1
+#     return count
+
+# n = float(input('Введите точность d = '))
+# print(f'Pi  с заданой точностью = {round(math.pi, diskret(n))}')
+
+
+
+# 2 Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+# def prostoydel(num):
+   
+#     x = 2
+#     mnojetel = []
+#     while x * x <= num:
+#         while num % x == 0:
+#             mnojetel.append(x)
+#             num = num // x
+#         x =x + 1
+#     if x > 1:
+#         mnojetel.append(num)
+#     return mnojetel
+# num = int(input('Введите натуральное число: '))
+# print(prostoydel(num))
+
+
+# 3 Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов 
+# исходной последовательности.
+
+# def random_list(size: int, min_value = 0, max_value = 100):
+#     new_list = []
+#     for index in range(size + 1):
+#         new_list.append(randint(min_value, max_value))
+#     return new_list
+
+# random_list = random_list(10, 0 , 10)
+# print(random_list)
+
+# new_list =[]
+
+# for elem in random_list:
+#     count = 0
+#     for new_elem in random_list:
+#         if elem == new_elem: 
+#             count += 1
+#     if count == 1: new_list.append(elem)
+
+# print(new_list)
+
+
+
+# 4 Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100)
+#  многочлена и записать в файл многочлен степени k.
+# *Пример:*  k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+
+# def general_koef(n):
+
+#     mnogochlen = ""
+        
+#     for i in range(n, -1, -1):
+#         rnd = randint(0,10)
+#         if rnd != 0:
+#             if len(mnogochlen) > 1:
+#                 mnogochlen += " + "
+#             if i == 0:
+#                 mnogochlen += str(rnd)
+#             else:
+#                 mnogochlen += str(rnd) + "*x^" + str(i)
+#     mnogochlen += " = 0"
+#     return mnogochlen
+
+# def main():
+   
+#     polynom = general_koef(k)
+#     print("Случайный многочлен:", polynom)
+#     with open("dz4_5.txt", "w") as file1:
+#         file1.write(polynom)
+
+# k = int(input('Введите степень k: '))
+
+# if __name__ == "__main__":
+#     main()
+
+# 5 Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, 
+# содержащий сумму многочленов.
+
+
+# def readFromFile(_file):
+
+#     data = ""
+#     with open(_file) as rfile:
+#         data = rfile.read()
+#     return data
+
+# num_1 = readFromFile('dz4_4.txt')
+# num_2 = readFromFile('dz4_5.txt')
+# # print(num_1)
+# # print(num_2)
+# num_1 = num_1[:num_1.find('=')].split('+')
+# num_2 = num_2[:num_2.find('=')].split('+')
+# num_all = num_1 + num_2
+
+# my_dict = {}
+
+# for elem in num_all:
+#     if 'x' not in elem:
+#         key = 0
+#     elif '^' not in elem:
+#         key = 1
+#     else:
+#         key = int(elem[elem.find('^') + 1:])
+        
+#     if key > 0:
+#         if key in my_dict:
+#             my_dict[key] += int(elem[:elem.find('*')])
+#         else:
+#             my_dict[key] = int(elem[:elem.find('*')])
+#     else:
+#         if key in my_dict:
+#             my_dict[0] += int(elem)
+#         else:
+#             my_dict[0] = int(elem) 
+#     # print(my_dict)
+
+# my_str = '' 
+# for key, value in sorted(my_dict.items(),reverse=True):
+#     if key == 0:
+#         my_str += str(value)
+#     elif key == 1:
+#         my_str += f'{value}*x + '
+#     else:
+#         my_str += f'{value}*x^{key} + '
+    
+# my_str = my_str + ' = 0'
+# # print(my_str)
+# data = open('dz4_5sum.txt', 'w')
+# data.write(my_str)
+
+
+
+
